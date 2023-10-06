168d7d1504148b6ccf8f2b9b207b721c00ce018e
BobTheBu1ld3r <123880897+BobTheBu1ld3r@users.noreply.github.com>
2023-10-06 15:12:50 +0100

    Show example

---
+++ index.js
@@ -0,0 +1,9 @@
+import LinkedList from "./LinkedList.js";
+
+const linkedList = new LinkedList();
+
+linkedList.append(4);
+linkedList.append(6);
+linkedList.append(9);
+linkedList.append(3);
+linkedList.toString();
